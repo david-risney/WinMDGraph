29b01719543fd5c8ba4b459097537b576fdc07a8
Dave Risney <dave@deletethis.net>
2016-10-31 21:33:17 -0700

    initial commit

---
+++ Graph.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WinMDGraph
+{
+    public class Graph<NodeContext>
+    {
+        public class Edge : IEquatable<Edge>, IComparable<Edge>
+        {
+            public Edge(Graph<NodeContext> parent, Node startIn, Node endIn)
+            {
+                Parent = parent;
+                _Start = startIn;
+                _End = endIn;
+
+                // Make sure this member data is all setup before sending out references to this.
+                // If this isn't setup then ToString, Equals, etc won't work.
+                _Start.OutEdges.Add(this);
+                _End.InEdges.Add(this);
+            }
+            private Graph<NodeContext> Parent;
+            private Node _Start;
+            public Node Start
+            {
+                get
+                {
+                    return _Start;
+                }
+            }
+            private Node _End;
+            public Node End
+            {
+                get
+                {
+                    return _End;
+                }
+            }
+
+            public void Remove()
+            {
+                _Start.OutEdges.Remove(this);
+                _End.InEdges.Remove(this);
+                Parent.Edges.Remove(this);
+            }
+
+            public bool Equals(Edge other)
+            {
+                return this.ToString() == other.ToString();
+            }
+
+            public override bool Equals(object obj)
+            {
+                return obj is Edge && this.Equals((Edge)obj);
+            }
+            public int CompareTo(Edge other)
+            {
+                return this.ToString().CompareTo(other.ToString());
+            }
+
+            public override string ToString()
+            {
+                return "Edge(" + this.Start.ToString() + "," + this.End.ToString() + ")";
+            }
+
+            public override int GetHashCode()
+            {
+                return this.ToString().GetHashCode();
+            }
+        }
+
+        public class Node : IEquatable<Node>, IComparable<Node>
+        {
+            private Graph<NodeContext> Parent;
+            public Node(Graph<NodeContext> parent)
+            {
+                Parent = parent;
+            }
+            public HashSet<Edge> OutEdges = new HashSet<Edge>();
+            public HashSet<Edge> InEdges = new HashSet<Edge>();
+            public NodeContext Context;
+            public void RemoveAndCollapseAllEdges()
+            {
+                foreach (Edge outEdge in OutEdges.ToList())
+                {
+                    foreach (Edge inEdge in InEdges.ToList())
+                    {
+                        // outEdge this -> other
+                        // inEdge other -> this
+                        Parent.AddEdge(inEdge.Start, outEdge.End);
+                    }
+                }
+                RemoveAndRemoveAllEdges();
+            }
+
+            public void RemoveAndRemoveAllEdges()
+            {
+                OutEdges.ToList().ForEach(edge => edge.Remove());
+                InEdges.ToList().ForEach(edge => edge.Remove());
+                Parent.NodeContextToNode.Remove(this.Context);
+            }
+
+            public bool Equals(Node other)
+            {
+                return this.ToString() == other.ToString();
+            }
+
+            public override bool Equals(object obj)
+            {
+                return obj is Node && this.Equals(((Node)obj));
+            }
+
+            public override string ToString()
+            {
+                return "Node(" + Context.ToString() + ")";
+            }
+
+            public int CompareTo(Node other)
+            {
+                return this.ToString().CompareTo(other.ToString());
+            }
+
+            public override int GetHashCode()
+            {
+                return this.ToString().GetHashCode();
+            }
+        }
+
+        public Node GetOrAddNode(NodeContext context)
+        {
+            if (NodeContextToNode.ContainsKey(context))
+            {
+                return NodeContextToNode[context];
+            }
+            else
+            {
+                Node node = new Node(this);
+                node.Context = context;
+                NodeContextToNode[context] = node;
+                return node;
+            }
+        }
+
+        public Edge AddEdge(Node start, Node end)
+        {
+            Edge edge = new Edge(this, start, end);
+            Edges.Add(edge);
+            return edge;
+        }
+
+        public HashSet<Edge> Edges = new HashSet<Edge>();
+        public Dictionary<NodeContext, Node> NodeContextToNode = new Dictionary<NodeContext, Node>();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace WinMDGraph
+{
+    class Program
+    {
+        static string EncodeNameForDot(string raw)
+        {
+            return raw.Replace('`', '_').Replace('.', '_');
+        }
+
+        private class ParsedArgs
+        {
+            public ParsedArgs(string[] args)
+            {
+                for (int idx = 0; idx < args.Length; ++idx)
+                {
+                    switch (args[idx])
+                    {
+                        case "-file":
+                            ++idx;
+                            if (idx >= args.Length)
+                            {
+                                throw new ArgumentException("-match must be followed by a match pattern");
+                            }
+                            files.Add(args[idx]);
+                            break;
+                        case "-match":
+                            ++idx;
+                            if (idx >= args.Length)
+                            {
+                                throw new ArgumentException("-match must be followed by a match pattern");
+                            }
+                            matches.Add(args[idx]);
+                            break;
+                        default:
+                            throw new ArgumentException("Unknown parameter " + args[idx]);
+                    }
+                }
+            }
+            public List<string> files = new List<string>();
+            public List<string> matches = new List<string>();
+        }
+
+        static void Main(string[] args)
+        {
+            ParsedArgs parsedArgs = new ParsedArgs(args);
+            ConsoleWriteClassFactory(parsedArgs.files, parsedArgs.matches);
+        }
+
+        static Type NodeToType(Graph<WinMDTypes.TypeInfo>.Node node)
+        {
+            return node.Context.Type;
+        }
+
+        static string TypeInfoToFullName(WinMDTypes.TypeInfo typeInfo)
+        {
+            return typeInfo.Type.FullName;
+        }
+
+        static string NodeToNamespace(Graph<WinMDTypes.TypeInfo>.Node node)
+        {
+            return NodeToType(node).Namespace;
+        }
+
+        static string NodeToName(Graph<WinMDTypes.TypeInfo>.Node node)
+        {
+            return NodeToType(node).Name;
+        }
+
+        static string NodeToFullname(Graph<WinMDTypes.TypeInfo>.Node node)
+        {
+            return NodeToType(node).FullName;
+        }
+
+        static string NodeToDebugDescrption(Graph<WinMDTypes.TypeInfo>.Node node)
+        {
+            return node.Context.ToString();
+        }
+
+        static string EdgeToNamespace(Graph<WinMDTypes.TypeInfo>.Edge edge)
+        {
+            string ns1 = NodeToNamespace(edge.Start);
+            string ns2 = NodeToNamespace(edge.End);
+            return ns1 == ns2 ? ns1 : "";
+        }
+
+        static void ConsoleWriteClassFactory(List<string> files, List<string> matchesRaw)
+        {
+            List<Regex> matches = matchesRaw.Select<string, Regex>(matchRaw => new Regex(matchRaw)).ToList();
+            WinMDTypes typeStore = new WinMDTypes(files.ToArray());
+            var graph = typeStore.CreateGraph(typeInfo => {
+                string fullName = TypeInfoToFullName(typeInfo);
+                Type type = typeInfo.Type;
+                bool genericArgs = false;
+                if (type != null)
+                {
+                    genericArgs = type.GenericTypeArguments.Length > 0;
+                }
+
+                bool nameMatch = (fullName != null && matches.Any(match => match.IsMatch(fullName)));
+                return nameMatch || genericArgs;
+            });
+
+            Console.WriteLine("// All in graph to start");
+            foreach (var node in graph.NodeContextToNode.Values)
+            {
+                Console.WriteLine("// - " + NodeToDebugDescrption(node));
+            }
+
+            Console.WriteLine("// Collapse types that don't match any of the regex but are parameterized types");
+            foreach (var node in graph.NodeContextToNode.Values.ToList().Where(
+                node =>
+                    (NodeToFullname(node) == null || !matches.Any(match => match.IsMatch(NodeToFullname(node)))) &&
+                    (NodeToType(node) != null && NodeToType(node).GenericTypeArguments.Length > 0)
+            ))
+            {
+                Console.WriteLine("// - " + NodeToDebugDescrption(node));
+                node.RemoveAndCollapseAllEdges();
+            }
+
+            Console.WriteLine("// Remove types that don't match any of the regex");
+            foreach (var node in graph.NodeContextToNode.Values.ToList().Where(
+                node =>
+                    (NodeToFullname(node) == null || !matches.Any(match => match.IsMatch(NodeToFullname(node)))) &&
+                    (NodeToType(node) == null || NodeToType(node).GenericTypeArguments.Length == 0)
+            ))
+            {
+                Console.WriteLine("// - " + NodeToDebugDescrption(node));
+                node.RemoveAndRemoveAllEdges();
+            }
+
+            Console.WriteLine("// Collapse non-classes");
+            foreach (var node in graph.NodeContextToNode.Values.ToList().Where(
+                node => (node.Context.Class == null || node.Context.Class.FullName == null) //&& node.Context.Interface == null
+            ))
+            {
+                Console.WriteLine("// - " + NodeToDebugDescrption(node));
+                node.RemoveAndCollapseAllEdges();
+            }
+
+            // Remove self referential edges
+            foreach (var edge in graph.Edges.ToList().Where(
+                edge => NodeToFullname(edge.Start) == NodeToFullname(edge.End)
+            ))
+            {
+                edge.Remove();
+            }
+
+            Dictionary<string, List<string>> namespaceToEntries = new Dictionary<string, List<string>>();
+
+            foreach (var node in graph.NodeContextToNode.Values)
+            {
+                string ns = NodeToNamespace(node);
+                List<string> entries = namespaceToEntries.ContainsKey(ns) ? namespaceToEntries[ns] : (namespaceToEntries[ns] = new List<string>());
+                entries.Add(EncodeNameForDot(NodeToName(node)) + ";");
+            }
+
+            foreach (var edge in graph.Edges)
+            {
+                string ns = EdgeToNamespace(edge);
+                List<string> entries = namespaceToEntries.ContainsKey(ns) ? namespaceToEntries[ns] : (namespaceToEntries[ns] = new List<string>());
+                entries.Add(EncodeNameForDot(NodeToName(edge.Start)) + " -> " + EncodeNameForDot(NodeToName(edge.End)) + ";");
+            }
+
+            Console.WriteLine("digraph {");
+            Console.WriteLine("\tnode [ fontname = \"Segoe UI\" ];");
+            Console.WriteLine("\tnode [ shape = \"rectangle\" ];");
+            int keyIdx = 0;
+
+            foreach (var key in namespaceToEntries.Keys)
+            {
+                if (key != "")
+                {
+                    Console.WriteLine("\tsubgraph cluster_" + keyIdx + " {");
+                    ++keyIdx;
+                }
+
+                foreach (var entry in namespaceToEntries[key])
+                {
+                    Console.WriteLine((key == "" ? "\t" : "\t\t") + entry);
+                }
+
+                if (key != "")
+                {
+                    Console.WriteLine("\t\tlabel = \"" + key + "\";");
+                    Console.WriteLine("\t\tcolor = lightgrey;");
+                    Console.WriteLine("\t\tfontname = \"Segoe UI\";");
+                    Console.WriteLine("\t}");
+                }
+            }
+            Console.WriteLine("}");
+        }
+    }
+}
