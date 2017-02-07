3841a2ea54ce87321563555967eae7ce55a5afad
Dave Risney <dave@deletethis.net>
2017-02-07 08:18:46 -0800

    Add test for System.Exception unprojection issue

--- winmdlog/test.ps1
+++ winmdlog/test.ps1
@@ -6,63 +6,53 @@ $exe = (dir bin\*\winmdlog.exe | sort LastWriteTime -desc)[0].fullname;
 $idx = 0;
 
 @(
-# 0 Test covers: 
-#   - IUri unprojection
-#   - read only properties
-#   - static methods
-#   - activatable class
+	#   - IUri unprojection
+	#   - read only properties
+	#   - static methods
+	#   - activatable class
     "-match Windows.Foundation.Uri",
 
-# 1 Test covers:
-#   - IReadOnlyList/IReadOnlyCollection/IVectorView unprojection
-#   - IIterable/IEnumerable unprojection
-#   - non activatable class
+	#   - IReadOnlyList/IReadOnlyCollection/IVectorView unprojection
+	#   - IIterable/IEnumerable unprojection
+	#   - non activatable class
     "-match Windows.Foundation.WwwFormUrlDecoder",
 
-# 2 Test covers:
-#   - events
-#   - AsyncOperation
+	#   - events
+	#   - AsyncOperation
     "-match Windows.System.RemoteSystems.RemoteSystemSessionController",
 
-# 3 Test covers:
-#   - IDisposable/ICloseable unprojection
+	#   - IDisposable/ICloseable unprojection
     "-match Windows.Devices.Adc.AdcChannel",
 
-# 4 Test covers:
-#   - System.TimeSpan unprojection
+	#   - System.TimeSpan unprojection
     "-match Windows.Devices.Gpio.GpioPin",
 
-# 5 Test covers:
-#   - Method parameter that is a reference
+	#   - Method parameter that is a reference
     "-match Windows.Devices.Gpio.GpioController",
 
-# 6 Test covers:
-#   - IList unprojection
+	#   - IList unprojection
     "-match Windows.Devices.Gpio.GpioChangeReader",
 
-# 7 Test covers:
-#   - Static only class
+	#   - Static only class
     "-match Windows.Devices.Custom.KnownDeviceTypes",
 
-# 8 Test covers:
-#	- DateTime unprojection
+	#	- DateTime unprojection
 	"-match Windows.Devices.SmartCards.SmartCardPinResetRequest",
 
-# 9 Test covers:
-#	- Nullable unprojection
+	#	- Nullable unprojection
 	"-match Windows.Devices.Power.BatteryReport",
 
-# 10 Test covers:
-#	- IDictionary unprojection
+	#	- IDictionary unprojection
 	"-match Windows.Devices.Sms.SmsWapMessage",
 
-# 11 Test covers:
-#	- IReadOnlyDictionary unprojection
+	#	- IReadOnlyDictionary unprojection
 	"-match Windows.Devices.AllJoyn.AllJoynAboutDataView",
 
-# 12 Test covers:
-#	- IKeyValuepair unprojection
-	"-match Windows.Devices.PointOfService.ClaimedBarcodeScanner"
+	#	- IKeyValuepair unprojection
+	"-match Windows.Devices.PointOfService.ClaimedBarcodeScanner",
+
+	#	- System.Exception unprojection
+	"-match Windows.ApplicationModel.PackageStagingEventArgs"
 ) | %{
     $outPath = "tests\$idx.out.actual";
     if ($Apply) {
